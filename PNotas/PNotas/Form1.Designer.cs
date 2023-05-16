0dc5ff8688c0245af26dc79db26c1379b997b16b
ArthurMotter <125912899+ArthurMotter@users.noreply.github.com>
2023-05-16 11:00:49 -0300

    Prova LP2 pasta

---
+++ PNotas/PNotas/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstBoxLista = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(60, 158);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(156, 108);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.BtnVerificar_Click);
+            // 
+            // lstBoxLista
+            // 
+            this.lstBoxLista.FormattingEnabled = true;
+            this.lstBoxLista.Location = new System.Drawing.Point(544, 50);
+            this.lstBoxLista.Name = "lstBoxLista";
+            this.lstBoxLista.Size = new System.Drawing.Size(120, 95);
+            this.lstBoxLista.TabIndex = 1;
+            this.lstBoxLista.SelectedIndexChanged += new System.EventHandler(this.LstBoxLista_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstBoxLista);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstBoxLista;
+    }
+}
+
---
+++ PNotas/PNotas/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PNotas
+{
+    public partial class Form1 : Form
+    {
+        char[,] matrizNota = new char[15, 10];
+        char[] vetorRespostas = new char[10] { 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E' };
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnVerificar_Click(object sender, EventArgs e)
+        {
+            string texto;
+           
+            for(int i = 0; i < 15; i++)
+            {
+
+                for(int j = 0; j < 10; j++)
+                {
+                    texto = Interaction.InputBox("digite uma letra", "lista de notas");
+                     if((texto.ToUpper()).Equals("A") || (texto.ToUpper()).Equals("B") ||
+                        (texto.ToUpper()).Equals("C") || (texto.ToUpper()).Equals("D") ||
+                        (texto.ToUpper()).Equals("E"))
+                    {
+                        matrizNota[i, j] = Convert.ToChar(texto);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Entrada invalida");
+                        j--;
+                    }
+                    for(int k = 0; k < 15; k++)
+                    {
+                        for( int l = 0; l < 10; l++)
+                        {
+                            if(matrizNota[k,l].Equals(vetorRespostas[l]))
+                            {
+                                MessageBox.Show("foi");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void LstBoxLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if(matrizNota[i,j] == vetorRespostas[j])
+                    {
+                        lstBoxLista.Text = "o aluno " + i + " acertou a questão " + j + " era "
+                        + vetorRespostas[j] + " escolheu " + matrizNota[i, j];
+                    }
+                    else
+                    {
+                        lstBoxLista.Text = "o aluno " + i + " errou a questão " + j + " era "
+                        + vetorRespostas[j] + " escolheu " + matrizNota[i, j];
+                    }
+                }
+            }
+        }
+    }
+}
